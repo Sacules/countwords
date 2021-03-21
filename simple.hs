66aa03bc82b24ec22bda20a0c2ee125b39fbe4dd
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-03-21 20:48:50 -0300

    slight optimizations

--- simple.hs
+++ simple.hs
@@ -1,25 +1,31 @@
 module Main where
 
+import System.IO
 import Text.Printf
 import Data.List
 import Data.Char
-import qualified Data.Map as M
+import qualified Data.HashMap.Strict as M
 
-count :: [String] -> M.Map String Int -> M.Map String Int
-count xs m = foldl (\ m x -> M.insertWith (+) x 1 m) m xs
+count :: [String] -> M.HashMap String Int -> M.HashMap String Int
+count xs m = foldl' f m xs
+    where f m x = M.insertWith (+) x 1 m
 
-countWords :: String -> M.Map String Int
-countWords line = count (words line) M.empty
+countWords :: String -> M.HashMap String Int
+countWords line = count w M.empty
+    where w = words line
 
 printCount :: [(String, Int)] -> IO ()
-printCount [] = putStr ""
+printCount [] = putStrLn ""
 printCount ((a,b):xs) = do
     putStrLn (printf "%s %d" a b)
     printCount xs
 
 main :: IO ()
 main = do
+    hSetBuffering stdin (BlockBuffering (Just (1024 * 64)))
     contents <- getContents
-    let counted = countWords (map toLower contents)
+    let lowered = map toLower contents
+        counted = countWords lowered
         sorted = sortBy (\ (_,a) (_,b) -> compare b a) $ M.toList counted
     printCount sorted
+
